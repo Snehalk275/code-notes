2e1297366fa97dc5a1bb6dd11f8f964b9e5265e8
Snehal Kothawade <skothawade120@gmail.com>
2025-02-27 14:32:08 +0530

    Update Largest among 3.java

--- Largest among 3.java
+++ Largest among 3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class swap{
+public class Largestamong3{
    public static void main(String [] args){
     int n1,n2,n3,largest;
         Scanner sc=new Scanner(System.in);
